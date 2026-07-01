42e669ba37a247bd615dd9153c41cb4af8f772ac
Emi-maker-sys <Bastidaperez.emiliano@gmail.com>
2026-06-30 18:25:40 -0600

    ahora el bot habla

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ from discord.ext import commands
 import random
 from dotenv import load_dotenv
 import os
+import pyttsx3
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
+engine = pyttsx3.init()
+engine.setProperty("rate", 150)
+engine.setProperty("vulume", 1.0)
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 datos = [
-    "🌊Los oceanos absorben más del 90% del calor generado en la Tierra.",
-    "🧊Las capas de hielo de Groenlandia y la Antártida pierden cientos de miles de millones de toneladas de hielo al año.",
+    "Los oceanos absorben más del 90% del calor generado en la Tierra.",
+    "Las capas de hielo de Groenlandia y la Antártida pierden cientos de miles de millones de toneladas de hielo al año.",
     "El rendimiento agrícola mundial podría disminuir hasta un 30% para 2050 si no se toman medidas.",
-    "🌊El agua marina se ha vuelto un 30% más ácida desde la Revolución Industrial, afectando corales y vida marina.",
-    "🦠El clima extremo propicia la expansión de enfermedades transmitidas por vectores, como el paludismo y el dengue."
+    "El agua marina se ha vuelto un 30% más ácida desde la Revolución Industrial, afectando corales y vida marina.",
+    "El clima extremo propicia la expansión de enfermedades transmitidas por vectores, como el paludismo y el dengue."
 ]
+
+def speak (text):
+    engine.say(text)
+    engine.runAndWait()
 
 @bot.event
 async def on_ready():
@@ -26,6 +35,9 @@ async def on_ready():
 @bot.command()
 async def dato(ctx):
     """Envía un dato curioso aleatorio."""
-    await ctx.send(random.choice(datos))
+
+    text = random.choice(datos)
+    await ctx.send(text)
+    speak(text)
 
 bot.run(TOKEN)
